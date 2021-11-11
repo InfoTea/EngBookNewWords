a17762c52041e85284b6f07ae1d0eea97036d79e
InfoTea <psyproffy@gmail.com>
2021-11-11 15:40:36 +0300

    will do Telegram bot

--- main.py
+++ main.py
@@ -30,41 +30,52 @@ def take_unknown_words():
 
     def sort_unknown_dict():
         word_dict_sort = sorted(take_unknown_dict().items(), key=lambda x: x[1], reverse=True)
-        f = open('ToLearn.txt', 'w')
+        file = open('ToLearn.txt', 'w')
         for key, value in word_dict_sort:
-            f.write(f'{key}: {value}' + '\n')
-        f.close()
+            file.write(f'{key}: {value}' + '\n')
+        file.close()
 
     sort_unknown_dict()
 
 
-take_unknown_words()
-# f = open('D:\Token.txt', 'r')
-# token = str(f.read())
-# f.close()
-# bot = telebot.TeleBot(token)
-#
-#
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.send_message(message.chat.id,
-#                      'Здравствуй, дорогой друг! Я помогаю тебе читать книги на английском языке. '
-#                      'Если ты загрузишь в меня книгу и свой словарный запас, '
-#                      'то я пришлю тебе все незнакомые слова в порядке частоты их попдания в тексте. '
-#                      'Подробнее принцип моей работы можно узнать по команде /help')
-#
-#
-# @bot.message_handler(commands=['start', 'help'])
-# def start(message):
-#     bot.send_message(message.chat.id, 'Hi, man!')
-#
-#
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     if message.text == 'Диана':
-#         bot.reply_to(message, 'Богиня')
-#     else:
-#         bot.reply_to(message, 'Пошёл на хуй!')
-#
-#
-# bot.polling()
+# take_unknown_words()
+f = open('D:\Token.txt', 'r')
+token = str(f.read())
+f.close()
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,
+                     'Здравствуй, дорогой друг! Я помогаю тебе читать книги на английском языке. '
+                     'Если ты загрузишь в меня книгу и свой словарный запас, '
+                     'то я пришлю тебе все незнакомые слова в порядке частоты их попдания в тексте. '
+                     'Подробнее принцип моей работы можно узнать по команде /help')
+
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id,
+                     'Список команд для бота:\n'
+                     '/start - начать работу с ботом\n'
+                     '/about_bot - подробная информация о боте\n'
+                     '/how_work - инструкция по работе с ботом\n'
+                     '/upload_book - передать боту английскую книгу (только в формате txt)\n'
+                     '/upload_dictionary - передать боту свой словарный запас (только в формате txt) '
+                     '\u2757заменит предыдущий!\u2757\n'
+                     '/mark_words - отметить уже изученные слова из последнего словаря\n'
+                     '/download_dictionary - выгрузить свой словарный запас\n'
+                     '/download_unknown_worlds - выгрузить незнакомые слова из последней книги\n'
+                     '/buy_superuser_key - оформить подписку на регулярное пользование ботом\n'
+                     '/how_much_words - узнать сколько слов в твоём словарном запасе\n'
+                     '/stop - остановить работу бота')
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.text:
+        bot.reply_to(message, 'Воспользуйтесь командой /help для просмотра доступных команд')
+
+
+bot.polling()
